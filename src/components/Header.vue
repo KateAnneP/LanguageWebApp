1c1455aee32e6fc2a5032ab573ccff098a0f20e2
Katarzyna Pieczonka <katpieczonka@gmail.com>
2023-04-25 15:07:41 +0200

    Start of project

---
+++ src/components/Header.vue
@@ -0,0 +1,101 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="logo">
+      <img scr="" />
+  </div>
+    <navbar>
+        <div class="dropdown">
+            <div class="dropbtn">Sign in</div>
+        </div>
+        <div class="dropdown">
+            <button class="dropbtn">Dropdown</button>
+            <div class="dropdown-content">
+                <a href="#">Vocabulary</a>
+                <a href="#">Langauges</a>
+                <a href="#">Categories</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <div class="dropbtn">Practise</div>
+            <div class="dropdown-content">
+                <a href="#">Write words</a>
+                <a href="#">Quiz</a>
+            </div>
+        </div>
+    </navbar>
+
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+
+/* Dropdown Button */
+.dropbtn {
+    background-color: #04AA6D;
+    color: white;
+    padding: 16px;
+    font-size: 16px;
+    border: none;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f1f1f1;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {background-color: #ddd;}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {display: block;}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropbtn {background-color: #3e8e41;}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+//import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 import './assets/main.css'
+//import "bootstrap/dist/css/bootstrap.css";
+//import "bootstrap-vue/dist/bootstrap-vue.css";
+
+//app.use(BootstrapVue);
+//app.use(IconsPlugin);
 
 const app = createApp(App)
 
